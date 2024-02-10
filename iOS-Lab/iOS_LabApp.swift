b84d7f1a88545a422f33ba7c414e60641b330891
hsuuuk <hsuuuk.dev@gmail.com>
2024-02-10 12:51:03 +0900

    Initial Commit

---
+++ iOS-Lab/iOS_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_LabApp.swift
+//  iOS-Lab
+//
+//  Created by 심현석 on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct iOS_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
