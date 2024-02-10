c990270a8203751d601c8b904fad52b949b916a2
hsuuuk <hsuuuk.dev@gmail.com>
2024-02-10 13:15:24 +0900

    first commit

--- iOS-Lab/iOS_LabApp.swift
+++ iOS-Lab/iOS_LabApp.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 @main
-struct iOS_LabApp: App {
+struct SwiftLabApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ViewControllerView()
         }
     }
 }
+
+struct ViewControllerView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UINavigationController
+
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        let vc = BottomSheetCustom()
+        let nav = UINavigationController(rootViewController: vc)
+        return nav
+    }
+
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+    }
+}
+
+//struct ViewControllerView: UIViewControllerRepresentable {
+//    typealias UIViewControllerType = TicketTimerTutorial_TabBarController
+//
+//    func makeUIViewController(context: Context) -> UIViewControllerType {
+//        let vc = TicketTimerTutorial_TabBarController()
+//        return vc
+//    }
+//
+//    func updateUIViewController(_ uiViewController: TicketTimerTutorial_TabBarController, context: Context) {
+//    }
+//}
